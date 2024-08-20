55b2e91d57b1c10dc48f8a46a1d149175cc6c858
dhruvpurohit2k <purohitdhruv00@gmail.com>
2024-08-20 23:00:50 +0530

    Added a more efficient code of sqrt of x

--- leetcode/sqrt(x).java
+++ leetcode/sqrt(x).java
@@ -10,3 +10,24 @@ class Solution {
         return 0;
     }
 }
+//This is a much more efficient code which utilises the concept of binary search to get to logn time
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+        int low = 1;
+        int high = x;
+        int mid = -1;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if ((long) mid * mid > (long) x) {
+                high = mid - 1;
+            } else if (mid * mid == x) {
+                return mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+}
