3a2ccd3f1ca1c93a2c055b55256f331f7334a648
dhruvpurohit2k <purohitdhruv00@gmail.com>
2024-08-22 18:59:54 +0530

    LeetCode ques : Merge Sorted Array with better solution.

--- leetcode/merege-sorted-array.java
+++ leetcode/merege-sorted-array.java
@@ -35,3 +35,19 @@ class Solution {
     }
 
 }
+//This is better solution. It makes 3 pointers. One at the end of nums1's final number, other at the end of nums2 and third at the end of nums1. Since both the arrays are already sorted in ascending order we simply compare the first two pointer and push the smallest value to the third pointer;
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n -1;
+        int k = m+n-1;
+        while(j>=0){
+            if(i>=0&&nums1[i]>nums2[j]){
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+}
