bff40503cfaaa2cb8b8f08a9bff47e9883e82ffd
dhruvpurohit2k <purohitdhruv00@gmail.com>
2024-09-07 22:09:56 +0530

    LeetCode ques - Add Digits

---
+++ leetcode/add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+     int sum = 0, current;
+     while(num/10!=0){
+        sum = 0;
+        current = num;
+        while(current!=0){
+            sum+=current%10;
+            current/=10;
+        }
+        num = sum;
+     }
+     return num;   
+    }
+}
